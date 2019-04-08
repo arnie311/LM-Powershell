efe5088a6efe13b55d93e5cf0bf61c56b3ea5b7c
Jonathan <jarno23@my.wgu.edu>
2019-04-08 16:40:18 -0500

    updates

--- Powershell Datasource Template.ps1
+++ Powershell Datasource Template.ps1
@@ -17,7 +17,7 @@ Remove-Variable * -ErrorAction SilentlyContinue
 $wmi_pass = "##WMI.PASS##"
 $wmi_user = "##WMI.USER##"
 $hostname = "##SYSTEM.SYSNAME##"
-$collectorName = hostname.exe
+$collectorName = hostname
 
 # Insert additional variables here
 
@@ -33,43 +33,45 @@ function Invoke-Discovery {
     param (
         [Parameter(position = 0, Mandatory = $true)]
         [ScriptBlock]$scriptblock,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 1, Mandatory = $true)]
         $wmi_user,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 2, Mandatory = $true)]
         $wmi_pass,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 3, Mandatory = $true)]
         $hostname,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 4, Mandatory = $true)]
         $collectorName
     )
-
-    #-----Determin the type of query to make-----
-    # check to see if this is monitoring the collector
-    if ($hostname -eq $collectorName) {
-        $response = Invoke-Command -ScriptBlock $scriptBlock
+    try {
+        #-----Determin the type of query to make-----
+        # check to see if this is monitoring the collector
+        if ($hostname -like $collectorName) {
+            $response = Invoke-Command -ScriptBlock $scriptBlock
+        }
+        # are wmi user/pass set -- e.g. are these device props either not substiuted or blank
+        elseif (([string]::IsNullOrWhiteSpace($wmi_user) -or $wmi_user -like "WMI" -or !$wmi_user) -and `
+            ([string]::IsNullOrWhiteSpace($wmi_pass)) -or $wmi_pass -like "WMI" -or !$wmi_pass) {
+            # no
+            $response = Invoke-Command -ComputerName $hostname -ScriptBlock $scriptBlock
+        }
+        else {
+            # yes. convert user/password into a credential string
+            $remote_pass = ConvertTo-SecureString -String $wmi_pass -AsPlainText -Force;
+            $remote_credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $wmi_user, $remote_pass;
+            $response = Invoke-Command -ComputerName $hostname -Credential $remote_credential -ScriptBlock $scriptBlock
+        }
     }
-    # are wmi user/pass set -- e.g. are these device props either not substiuted or blank
-    elseif ([string]::IsNullOrWhiteSpace($wmi_user) -and [string]::IsNullOrWhiteSpace($wmi_pass)) {
-        # no
-        $response = Invoke-Command -ComputerName $hostname -ScriptBlock $scriptBlock
-    }
-    else {
-        # yes. convert user/password into a credential string
-        $remote_pass = ConvertTo-SecureString -String $wmi_pass -AsPlainText -Force;
-        $remote_credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $wmi_user, $remote_pass;
-        $response = Invoke-Command -ComputerName $hostname -Credential $remote_credential -ScriptBlock $scriptBlock
+    catch {
+        # exit code of non 0 will mean the script failed and not overwrite the instances that have already been found
+        throw $Error[0].Exception
+        exit 1
     }
     return $response
 }
 
-try {
-    Invoke-Discovery -scriptblock $scriptBlock -wmi_user $wmi_user -wmi_pass $wmi_pass -hostname $hostname -collectorName $collectorName
-    exit 0
-}
-catch {
-    # exit code of non 0 will mean the script failed and not overwrite the instances that have already been found
-    exit 1
-}
+Invoke-Discovery $scriptBlock $wmi_user $wmi_pass $hostname $collectorName
+exit 0
+
 
 
 
@@ -92,7 +94,7 @@ Remove-Variable * -ErrorAction SilentlyContinue
 $wmi_pass = "##WMI.PASS##"
 $wmi_user = "##WMI.USER##"
 $hostname = "##SYSTEM.SYSNAME##"
-$collectorName = hostname.exe
+$collectorName = hostname
 $wildValue = "##WILDVALUE##"
 
 # Insert additional variables here
@@ -109,41 +111,42 @@ function Invoke-DataCollection {
     param (
         [Parameter(position = 0, Mandatory = $true)]
         [ScriptBlock]$scriptblock,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 1, Mandatory = $true)]
         $wmi_user,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 2, Mandatory = $true)]
         $wmi_pass,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 3, Mandatory = $true)]
         $hostname,
-        [Parameter(position = 0, Mandatory = $true)]
+        [Parameter(position = 4, Mandatory = $true)]
         $collectorName
     )
-
-    #-----Determin the type of query to make-----
-    # check to see if this is monitoring the collector
-    if ($hostname -eq $collectorName) {
-        $response = Invoke-Command -ScriptBlock $scriptBlock
+    try {
+        #-----Determin the type of query to make-----
+        # check to see if this is monitoring the collector
+        if ($hostname -like $collectorName) {
+            $response = Invoke-Command -ScriptBlock $scriptBlock
+        }
+        # are wmi user/pass set -- e.g. are these device props either not substiuted or blank
+        elseif (([string]::IsNullOrWhiteSpace($wmi_user) -or $wmi_user -like "WMI" -or !$wmi_user) -and `
+            ([string]::IsNullOrWhiteSpace($wmi_pass)) -or $wmi_pass -like "WMI" -or !$wmi_pass) {
+            # no
+            $response = Invoke-Command -ComputerName $hostname -ScriptBlock $scriptBlock
+        }
+        else {
+            # yes. convert user/password into a credential string
+            $remote_pass = ConvertTo-SecureString -String $wmi_pass -AsPlainText -Force;
+            $remote_credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $wmi_user, $remote_pass;
+            $response = Invoke-Command -ComputerName $hostname -Credential $remote_credential -ScriptBlock $scriptBlock
+        }
     }
-    # are wmi user/pass set -- e.g. are these device props either not substiuted or blank
-    elseif ([string]::IsNullOrWhiteSpace($wmi_user) -and [string]::IsNullOrWhiteSpace($wmi_pass)) {
-        # no
-        $response = Invoke-Command -ComputerName $hostname -ScriptBlock $scriptBlock
-    }
-    else {
-        # yes. convert user/password into a credential string
-        $remote_pass = ConvertTo-SecureString -String $wmi_pass -AsPlainText -Force;
-        $remote_credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $wmi_user, $remote_pass;
-        $response = Invoke-Command -ComputerName $hostname -Credential $remote_credential -ScriptBlock $scriptBlock
+    catch {
+        # exit code of non 0 will mean the script failed and not overwrite the instances that have already been found
+        throw $Error[0].Exception
+        exit 1
     }
     return $response
 }
 
-try {
-    Invoke-DataCollection -scriptblock $scriptBlock -wmi_user $wmi_user -wmi_pass $wmi_pass -hostname $hostname -collectorName $collectorName
-    exit 0
-}
-catch {
-    # exit code of non 0 will mean the script failed and all datapoints will show as NaN
-    exit 1
-}
+Invoke-DataCollection $scriptBlock $wmi_user $wmi_pass $hostname $collectorName
+exit 0
 
